0a705ecf747d23472a4f9b4081b4254c45fd1443
Benjamin Fattori <46356259+fattorib@users.noreply.github.com>
2020-08-22 12:32:07 -0400

    Test accuracy: 73%

--- Conv/model.py
+++ Conv/model.py
@@ -48,16 +48,16 @@ class Model(nn.Module):
         #Depth here is the number of feature maps we had previously passed
         self.conv2 = nn.Conv2d(32, 64, 3,stride = 1, padding = 1)
         
-        self.conv3 = nn.Conv2d(64,100,3, stride = 1, padding = 1)
+        self.conv3 = nn.Conv2d(64,128,3, stride = 1, padding = 1)
         
-        
+        self.conv4 = nn.Conv2d(128,256,3, stride = 1, padding = 1)
         
         #Pooling layer with size of 2 and stride of 2. Halving the xy size.
         self.pool = nn.MaxPool2d(2,2)
         
         
-        #Final image size is x_final*y_final*depth. In our case this is 75*75*32
-        self.fc1 = nn.Linear(16*16*100,256)
+        #Final image size is x_final*y_final*depth
+        self.fc1 = nn.Linear(8*8*256,256)
         
         self.fc2 = nn.Linear(256,10)
         
@@ -67,9 +67,11 @@ class Model(nn.Module):
         x = F.relu(self.pool(self.conv2(x)))
         x = F.relu(self.pool(self.conv3(x)))
         
-        x = x.view(-1,16*16*100)
+        x = F.relu(self.pool(self.conv4(x)))
         
-        x = self.fc1(x)
+        x = x.view(-1,8*8*256)
+        
+        x = F.relu(self.fc1(x))
         
         x = self.fc2(x)
         
